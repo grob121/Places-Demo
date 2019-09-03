f143c7f2f7b6091c39a79e7e3a2c07806ab832bd
Allan Pagdanganan <pagdanganan.allan@gmail.com>
2019-09-03 09:49:35 +0800

    Initial Commit

---
+++ Places Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places Demo
+//
+//  Created by Allan Pagdanganan on 03/09/2019.
+//  Copyright © 2019 Allan Pagdanganan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Places DemoTests/Places_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Places_DemoTests.swift
+//  Places DemoTests
+//
+//  Created by Allan Pagdanganan on 03/09/2019.
+//  Copyright © 2019 Allan Pagdanganan. All rights reserved.
+//
+
+import XCTest
+@testable import Places_Demo
+
+class Places_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
