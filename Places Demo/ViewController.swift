fff7f6d33b34d08e6d7216f854f74518128338c3
Allan Pagdanganan <pagdanganan.allan@gmail.com>
2019-09-03 12:43:34 +0800

    Set up map and Display user location

--- Places Demo/ViewController.swift
+++ Places Demo/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        
+        requestLocationAuthorization()
+        getUserLocation()
     }
+    
+    
+    // MARK: - Location Manager
+    
+    /**
+     Request permission if your app's authorization status is not yet determined.
+     
+     Calling this method lets the user choose whether to grant the request for always authorization or to leave your app with when-in-use authorization.
+     */
+    func requestLocationAuthorization() {
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
 
-
+    func getUserLocation() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locationValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        
+        let centerCoordinate = CLLocationCoordinate2D(latitude: locationValue.latitude, longitude: locationValue.longitude)
+        let region = MKCoordinateRegion(center: centerCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
+    }
+    
 }
 
